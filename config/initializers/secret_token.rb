c56a7fcbd54316c304a5fcd0bc02bc6348462c0c
robin <robinskilling@gmail.com>
2012-02-02 15:47:25 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lanadelrey::Application.config.secret_token = '4f7e8c38c3eefe0733b95ddf341209934558b7cb6349966ed72cac5426b42d2b1f930694cf309983ec7ee6069a5dde9e4102f17f8bc3797bc846f09fef1fb605'
