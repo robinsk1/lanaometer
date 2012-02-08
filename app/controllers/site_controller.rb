64d7c8b005f6b2bb241d7a6a0bc0a4bca034c840
robin <robinskilling@gmail.com>
2012-02-08 16:11:14 +0100

    change to 50

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -5,7 +5,7 @@ class SiteController < ApplicationController
   search_term = "lana del rey"
   @articles = GuardianContent::Content.search(search_term)
   senti = Sentiment.new
-  @results = senti.get_tweet_hash( search_term, 100)
+  @results = senti.get_tweet_hash( search_term, 50)
   negative, neutral, positive = 0, 0, 0
   for tweet in @results do
     sentiment = senti.analyze_sentiment( tweet['text'] )
